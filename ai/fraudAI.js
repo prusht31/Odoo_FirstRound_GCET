3d8f5188e566f45e450344107c0d108458e52c1b
prusht31 <23dce008@charusat.edu.in>
2026-01-03 11:20:24 +0530

    Delete ai directory

--- ai/fraudAI.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(records){
-  let flags = [];
-  records.forEach(r=>{
-    if(r.checkIn && r.checkIn.includes("12:"))
-      flags.push(r);
-  });
-  return flags;
-};
--- ai/leaveAI.js
+++
@@ -1,9 +0,0 @@
-function predictLeave(leaveCount, avgAttendance, reasonLength){
-  let score = 0;
-  if(leaveCount < 3) score += 40;
-  if(avgAttendance > 85) score += 40;
-  if(reasonLength > 20) score += 20;
-  return Math.min(score,100);
-}
-
-module.exports = predictLeave;
