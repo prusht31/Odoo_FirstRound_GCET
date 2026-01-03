5c05280611f117f0b5b4335a20513b7119662409
prusht31 <23dce008@charusat.edu.in>
2026-01-03 14:50:21 +0530

    Delete backend directory

--- backend/controllers/aiController.js
+++
@@ -1,8 +0,0 @@
-const Attendance = require("../models/Attendance");
-const fraudAI = require("../ai/fraudAI");
-
-exports.detectFraud = async(req,res)=>{
-  const data = await Attendance.find();
-  const suspicious = fraudAI(data);
-  res.json(suspicious);
-};
--- backend/middleware/roleMiddleware.js
+++
@@ -1,5 +0,0 @@
-module.exports = role => (req,res,next)=>{
-  if(req.user.role !== role)
-    return res.status(403).json({msg:"Access Denied"});
-  next();
-};
